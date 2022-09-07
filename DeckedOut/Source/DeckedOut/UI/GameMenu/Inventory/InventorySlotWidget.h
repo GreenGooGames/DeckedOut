d810ed4702c35379225e1b576a2faeff2d01a0a1
Koen <40505667+Bursterker@users.noreply.github.com>
2022-09-07 17:59:36 +0100

    Common UI
    Basis of common UI implementation:
    - Styles
    - Stack
    - Keyboard/mouse and gamepad

---
+++ DeckedOut/Source/DeckedOut/UI/GameMenu/Inventory/InventorySlotWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "InventorySlotWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DECKEDOUT_API UInventorySlotWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
